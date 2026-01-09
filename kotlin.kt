d0706a6aec38ff458a8f79b575e208c98c614318
rcb269 <marvelstudiom64@gmail.com>
2026-01-10 01:25:52 +0530

    int byte short

--- kotlin.kt
+++ kotlin.kt
@@ -1,3 +1,23 @@
+// fun main(){
+//     println("hii")
+//     var Name="amit"
+//     Name="john" // type inference
+//     var age= 22
+//     age=25
+//     println("hello $Name your age is $age")
+// }
 fun main(){
-    println("hello world")
+    val integermaxvalue= Int.MAX_VALUE
+    val integerminvalue= Int.MIN_VALUE
+    println("interger max value is $integermaxvalue")
+    println("integer min value is $integerminvalue") 
+    val bytemaxvalue:Byte = Byte.MAX_VALUE
+    val byteminvalue:Byte = Byte.MIN_VALUE
+    println("byte max value is $bytemaxvalue")
+    println("byte min value is $byteminvalue")
+    val shortmaxvalue:Short = Short.MAX_VALUE
+    val shortminvalue:Short = Short.MIN_VALUE
+    println("short max value is $shortmaxvalue")
+    println("short min value is $shortminvalue")
+
 }
