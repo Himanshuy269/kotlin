e315e15e075c065de7d4ca1105e768bd07b350a7
rcb269 <marvelstudiom64@gmail.com>
2026-01-11 18:49:53 +0530

    doubleandfloat

--- kotlin.kt
+++ kotlin.kt
@@ -6,21 +6,37 @@
 //     age=25
 //     println("hello $Name your age is $age")
 // }
+// fun main(){
+//     val integermaxvalue= Int.MAX_VALUE
+//     val integerminvalue= Int.MIN_VALUE
+//     println("interger max value is $integermaxvalue")
+//     println("integer min value is $integerminvalue") 
+//     val bytemaxvalue:Byte = Byte.MAX_VALUE
+//     val byteminvalue:Byte = Byte.MIN_VALUE
+//     println("byte max value is $bytemaxvalue")
+//     println("byte min value is $byteminvalue")
+//     val shortmaxvalue:Short = Short.MAX_VALUE
+//     val shortminvalue:Short = Short.MIN_VALUE
+//     println("short max value is $shortmaxvalue")
+//     println("short min value is $shortminvalue")
+//     val longmaxvalue:Long = Long.MAX_VALUE
+//     val longminvalue:Long = Long.MIN_VALUE
+//     println("long max value is $longmaxvalue")
+//     println("long min value is $longminvalue")
+// }
+/*fun main(){
+    var x:Float=2.3f
+    println(x)//redutcant
+    var y:Double=2.3
+    println(y)//reductant
+    var a=2.3f
+    println(a)//inference
+    var b=2.3
+    println(b)//inference
+}*/
 fun main(){
-    val integermaxvalue= Int.MAX_VALUE
-    val integerminvalue= Int.MIN_VALUE
-    println("interger max value is $integermaxvalue")
-    println("integer min value is $integerminvalue") 
-    val bytemaxvalue:Byte = Byte.MAX_VALUE
-    val byteminvalue:Byte = Byte.MIN_VALUE
-    println("byte max value is $bytemaxvalue")
-    println("byte min value is $byteminvalue")
-    val shortmaxvalue:Short = Short.MAX_VALUE
-    val shortminvalue:Short = Short.MIN_VALUE
-    println("short max value is $shortmaxvalue")
-    println("short min value is $shortminvalue")
-    val longmaxvalue:Long = Long.MAX_VALUE
-    val longminvalue:Long = Long.MIN_VALUE
-    println("long max value is $longmaxvalue")
-    println("long min value is $longminvalue")
+    val x=4
+    val y=5
+    val ans=x+y
+    println("result=$ans")
 }
