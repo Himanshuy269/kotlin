433cab36f046e92eaf777cfbe4d3b8eae89cd4c0
rcb269 <marvelstudiom64@gmail.com>
2026-01-31 00:01:00 +0530

    done with for loop

---
+++ loop.kt
@@ -0,0 +1,21 @@
+//for loop
+/*fun main(){
+    for(i in 1..10){
+        println(i)
+    }
+}*/
+/*/ fun main(){
+     for(i in 1 until 10){
+         println(i)
+     }
+}*/
+/* fun main(){
+     for(i in 10 downTo 1){
+         println(i)
+     }
+ }*/
+/* fun main(){
+     for(i in 1 until 10 step 3){
+         println(i)
+     }
+ }*/
