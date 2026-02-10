20fee86822849553e7027ba9e7eed15b12456795
rcb269 <marvelstudiom64@gmail.com>
2026-02-10 18:38:17 +0530

    Class and object'

---
+++ OOPs.kt
@@ -0,0 +1,39 @@
+fun main(){
+    val car1=car()
+    car1.name = "tesla"
+    car1.model="s plaid"
+    car1.colors="red"
+    car1.doors=4
+    println( "Name=${car1.name}")
+    println( "model=${car1.model}")
+    println( "colors=${car1.colors}")
+    println( "doors=${car1.doors}")
+    car1.move()
+    car1.stop()
+
+    val car2=car()
+    car2.name = "ford"
+    car2.model="mustang"
+    car2.colors="blue"
+    car2.doors=4
+    println("\n")
+    println( "Name=${car2.name}")
+    println( "model=${car2.model}")
+    println( "colors=${car2.colors}")
+    println( "doors=${car2.doors}")
+    car2.move()
+    car2.stop()
+    
+}
+class car{
+    var name=""
+    var model=""
+    var colors=""
+    var doors=0
+    fun move(){
+        println("the car $name is moving")
+    }
+    fun stop(){
+        println("the car $name is moving")
+    }
+}
